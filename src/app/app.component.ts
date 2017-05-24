3ff8bce135e08043d43a3f3646ab5d5d42b213ae
manuelpimentel <manuelpimentel16@gmail.com>
2017-05-23 23:52:12 -0400

    Routing, averiguar como se hace en RC5+

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { ROUTER_CONFIGURATION } from '@angular/router';
+
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
 })
 export class AppComponent {
   title = 'app works!';
@@ -14,4 +17,10 @@ export class AppComponent {
   	belt: "Red"
   };
   
+  yell(e){
+    alert('i am yelling');
+    console.log(e);
+  }
+
+
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -9,9 +9,16 @@ export class HomeComponent implements OnInit {
   homeTitle = "El titulo de la pagina home";
 
   @Input() ninja;
+  @Output() onYell = new EventEmitter();
+
 
   alertMe(val){
   	alert(val);
+  }
+
+  fireYellEvent(e){
+    this.onYell.emit(e);
+
   }
 
   constructor() { }
